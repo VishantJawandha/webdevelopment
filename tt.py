46643185474cb844eceb4a20f43c79fed95d0a51
VishantJawandha <vishantsingh781@gmail.com>
2023-10-14 14:43:35 +0530

    all added

---
+++ tt.py
@@ -0,0 +1,25 @@
+
+
+
+def mySub(x,y):
+    w= x-y
+    return w
+
+
+
+# taking input
+
+print("give numbers")
+a= input()
+b= input()
+
+# type casting the numbers
+
+a= int(a)
+b= int(b)
+
+resultSub = mySub(a,b)
+print('Sub=' ,str(resultSub))
+
+
+# Scope: block of code   python...identation , java......curly braces
